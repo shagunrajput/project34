67159afc7016e00aad85e188ecf979c71a4bf0a5
shagunrajput <77843586+shagunrajput@users.noreply.github.com>
2021-05-23 21:10:58 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,29 @@
+class Box {
+  constructor(x, y, width, height,color) {
+    var options = {
+        'restitution':0.8,
+        'friction':1.0,
+        'density':20,
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.color=color;
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    fill (this.color);
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    strokeWeight(4);
+    stroke("black");
+    
+    //fill("purple");
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+}
